7c95e03029c6ed582d58232b32133b79069cfbff
mansur-sajid <mansursajid59@gmail.com>
2025-04-15 06:02:35 +0500

    dummy

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  devServer: {
-    host: '0.0.0.0',
-    port: 3000,
-  },
   /* config options here */
   env: {
     NEXTAUTH_URL: "http://34.224.67.13:3000",
+    NEXTAUTH_SECRET: "n6n3EdXvue3++hXvqTl+gxjy18fDSvtvuGyQuGehpj0=",
   },
 };
 
