bace23567013ee257fecbe8ff10f41ad879b1d78
mansur-sajid <mansursajid59@gmail.com>
2025-04-15 05:43:20 +0500

    third commit

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  devServer: {
+    host: '0.0.0.0',
+    port: 3000,
+  },
   /* config options here */
   env: {
-    NEXTAUTH_URL: "https://cdcd-51-36-232-117.ngrok-free.app",
+    NEXTAUTH_URL: "http://34.224.67.13:3000",
   },
 };
 
