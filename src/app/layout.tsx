24ee4ebc22b7b2ed1ac1de38601e3ea81f0f0750
mansur-sajid <mansursajid59@gmail.com>
2025-04-17 01:18:41 +0500

    chore: remove auth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,6 @@ import "./globals.css";
 import { useState } from "react";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import SessionProviderWrapper from "./session-provider"; // Import the new provider
 
 
 export default function RootLayout({
@@ -15,11 +14,10 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <QueryClientProvider client={queryClient}> {/* Add the query client provider */}
-        <SessionProviderWrapper>
           <nav className="p-4 flex justify-end">
           </nav>
           {children}
-        </SessionProviderWrapper>
+
         </QueryClientProvider>
       </body>
     </html>
