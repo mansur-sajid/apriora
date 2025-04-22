a80833dd46d436142eb5dcb7dfd1e257b47ae793
mansur-sajid <mansursajid59@gmail.com>
2025-04-23 02:15:10 +0500

    push build

---
+++ .next/server/app/(interview)/interview/[id]/completed/page.js
@@ -0,0 +1,201 @@
+(()=>{var e={};e.id=677,e.ids=[677],e.modules={3295:e=>{"use strict";e.exports=require("next/dist/server/app-render/after-task-async-storage.external.js")},4332:(e,t,r)=>{"use strict";r.d(t,{default:()=>l});var i=r(60687),o=r(43210),n=r(39091),s=r(8693),a=r(15497);function l({children:e}){let[t]=(0,o.useState)(()=>new n.E);return(0,i.jsx)(s.Ht,{client:t,children:(0,i.jsx)(a.u,{children:e})})}},7516:(e,t,r)=>{"use strict";r.r(t),r.d(t,{default:()=>n,metadata:()=>o});var i=r(37413);let o={title:"Interview Page"};function n({children:e}){return(0,i.jsx)("html",{lang:"en",children:(0,i.jsx)("body",{children:e})})}},7816:(e,t,r)=>{"use strict";r.r(t),r.d(t,{GlobalError:()=>s.a,__next_app__:()=>u,pages:()=>p,routeModule:()=>c,tree:()=>d});var i=r(65239),o=r(48088),n=r(88170),s=r.n(n),a=r(30893),l={};for(let e in a)0>["default","tree","pages","GlobalError","__next_app__","routeModule"].indexOf(e)&&(l[e]=()=>a[e]);r.d(t,l);let d={children:["",{children:["(interview)",{children:["interview",{children:["[id]",{children:["completed",{children:["__PAGE__",{},{page:[()=>Promise.resolve().then(r.bind(r,79805)),"/home/mirza-zeeshan/Apriora/apriora/src/app/(interview)/interview/[id]/completed/page.tsx"]}]},{}]},{layout:[()=>Promise.resolve().then(r.bind(r,7516)),"/home/mirza-zeeshan/Apriora/apriora/src/app/(interview)/interview/[id]/layout.tsx"]}]},{}]},{"not-found":[()=>Promise.resolve().then(r.t.bind(r,57398,23)),"next/dist/client/components/not-found-error"],forbidden:[()=>Promise.resolve().then(r.t.bind(r,89999,23)),"next/dist/client/components/forbidden-error"],unauthorized:[()=>Promise.resolve().then(r.t.bind(r,65284,23)),"next/dist/client/components/unauthorized-error"],metadata:{icon:[],apple:[],openGraph:[],twitter:[],manifest:"/manifest.webmanifest"}}]},{layout:[()=>Promise.resolve().then(r.bind(r,94431)),"/home/mirza-zeeshan/Apriora/apriora/src/app/layout.tsx"],"not-found":[()=>Promise.resolve().then(r.t.bind(r,57398,23)),"next/dist/client/components/not-found-error"],forbidden:[()=>Promise.resolve().then(r.t.bind(r,89999,23)),"next/dist/client/components/forbidden-error"],unauthorized:[()=>Promise.resolve().then(r.t.bind(r,65284,23)),"next/dist/client/components/unauthorized-error"],metadata:{icon:[],apple:[],openGraph:[],twitter:[],manifest:"/manifest.webmanifest"}}]}.children,p=["/home/mirza-zeeshan/Apriora/apriora/src/app/(interview)/interview/[id]/completed/page.tsx"],u={require:r,loadChunk:()=>Promise.resolve()},c=new i.AppPageRouteModule({definition:{kind:o.RouteKind.APP_PAGE,page:"/(interview)/interview/[id]/completed/page",pathname:"/interview/[id]/completed",bundlePath:"",filename:"",appPaths:[]},userland:{loaderTree:d}})},10846:e=>{"use strict";e.exports=require("next/dist/compiled/next-server/app-page.runtime.prod.js")},11997:e=>{"use strict";e.exports=require("punycode")},15497:(e,t,r)=>{"use strict";r.d(t,{I:()=>a,u:()=>s});var i=r(60687),o=r(43210);let n=(0,o.createContext)();function s({children:e}){let[t,r]=(0,o.useState)("recruiter");return(0,i.jsx)(n.Provider,{value:{role:t,changeRole:e=>{localStorage.setItem("role",e),r(e)}},children:e})}function a(){return(0,o.useContext)(n)}},16189:(e,t,r)=>{"use strict";var i=r(65773);r.o(i,"notFound")&&r.d(t,{notFound:function(){return i.notFound}}),r.o(i,"useParams")&&r.d(t,{useParams:function(){return i.useParams}}),r.o(i,"usePathname")&&r.d(t,{usePathname:function(){return i.usePathname}}),r.o(i,"useRouter")&&r.d(t,{useRouter:function(){return i.useRouter}})},19121:e=>{"use strict";e.exports=require("next/dist/server/app-render/action-async-storage.external.js")},27910:e=>{"use strict";e.exports=require("stream")},29294:e=>{"use strict";e.exports=require("next/dist/server/app-render/work-async-storage.external.js")},33508:(e,t,r)=>{Promise.resolve().then(r.t.bind(r,16444,23)),Promise.resolve().then(r.t.bind(r,16042,23)),Promise.resolve().then(r.t.bind(r,88170,23)),Promise.resolve().then(r.t.bind(r,49477,23)),Promise.resolve().then(r.t.bind(r,29345,23)),Promise.resolve().then(r.t.bind(r,12089,23)),Promise.resolve().then(r.t.bind(r,46577,23)),Promise.resolve().then(r.t.bind(r,31307,23))},33873:e=>{"use strict";e.exports=require("path")},39853:(e,t,r)=>{Promise.resolve().then(r.bind(r,44314))},40446:(e,t,r)=>{Promise.resolve().then(r.bind(r,63999))},44314:(e,t,r)=>{"use strict";r.d(t,{default:()=>i});let i=(0,r(12907).registerClientReference)(function(){throw Error("Attempted to call the default export of \"/home/mirza-zeeshan/Apriora/apriora/src/components/ClientWrapper.tsx\" from the server, but it's on the client. It's not possible to invoke a client function from the server, it can only be rendered as a Component or passed to props of a Client Component.")},"/home/mirza-zeeshan/Apriora/apriora/src/components/ClientWrapper.tsx","default")},45174:(e,t,r)=>{Promise.resolve().then(r.bind(r,79805))},55591:e=>{"use strict";e.exports=require("https")},61135:()=>{},63033:e=>{"use strict";e.exports=require("next/dist/server/app-render/work-unit-async-storage.external.js")},63364:(e,t,r)=>{Promise.resolve().then(r.t.bind(r,86346,23)),Promise.resolve().then(r.t.bind(r,27924,23)),Promise.resolve().then(r.t.bind(r,35656,23)),Promise.resolve().then(r.t.bind(r,40099,23)),Promise.resolve().then(r.t.bind(r,38243,23)),Promise.resolve().then(r.t.bind(r,28827,23)),Promise.resolve().then(r.t.bind(r,62763,23)),Promise.resolve().then(r.t.bind(r,97173,23))},63999:(e,t,r)=>{"use strict";r.r(t),r.d(t,{default:()=>a});var i=r(60687),o=r(64796),n=r(16189),s=r(43210);let a=()=>{let{id:e}=(0,n.useParams)(),t=(0,n.useRouter)(),{mutateAsync:r}=(0,o.Br)();return(0,s.useEffect)(()=>{(async()=>{try{let t=await r({interviewId:e});console.log("Interview Summary:",t)}catch(e){console.error("Error fetching interview summary:",e)}})()},[e,r]),(0,i.jsx)("div",{className:"flex flex-col items-center justify-center min-h-screen bg-gray-50 px-4",children:(0,i.jsxs)("div",{className:"max-w-xl bg-white shadow-lg rounded-2xl p-8 text-center",children:[(0,i.jsx)("h1",{className:"text-3xl font-semibold text-green-600 mb-4",children:"Thank You!"}),(0,i.jsx)("p",{className:"text-lg text-gray-700 mb-6",children:"We appreciate you taking the time to complete the interview."}),(0,i.jsx)("p",{className:"text-md text-gray-600 mb-6",children:"A detailed summary of your responses will be sent to the recruiter shortly."}),(0,i.jsx)("p",{className:"text-sm text-gray-600 underline cursor-pointer",onClick:()=>t.push("/"),children:"Go to home"})]})})}},64796:(e,t,r)=>{"use strict";r.d(t,{AG:()=>u,fi:()=>c,cr:()=>m,MO:()=>h,NH:()=>y,bC:()=>z,wc:()=>K,RB:()=>q,AY:()=>b,OD:()=>x,bE:()=>C,Br:()=>A,LF:()=>g,jb:()=>j,EL:()=>J,n6:()=>k});var i=r(54050),o=r(29494);class n extends Error{constructor(e="Validation Error",t=500,r={}){super(e),this.name=this.constructor.name,this.status=t,this.message=e,r&&(this.meta=r),Error.captureStackTrace?.(this,this.constructor)}}var s=r(99638),a=r(56002),l=r.n(a);let d=new s.l4("http://localhost:8000/graphql",{fetch:l(),credentials:"include"});function p(e,t,r){return async()=>d.request(e,t,r).catch(({response:e})=>{throw new n(e?.error,e?.status,{response:e})})}var u=function(e){return e.FourWeeksNotice="FOUR_WEEKS_NOTICE",e.Immediate="IMMEDIATE",e.ThreeWeeksNotice="THREE_WEEKS_NOTICE",e.TwoWeeksNotice="TWO_WEEKS_NOTICE",e}({}),c=function(e){return e.Contract="CONTRACT",e.FullTime="FULL_TIME",e.PartTime="PART_TIME",e}({}),m=function(e){return e.Hourly="HOURLY",e.Unit="UNIT",e}({}),h=function(e){return e.Archived="ARCHIVED",e.Draft="DRAFT",e.Published="PUBLISHED",e}({}),y=function(e){return e.Hybrid="HYBRID",e.OnSite="ON_SITE",e.Remote="REMOTE",e}({});let v=`
+    mutation CreateJobPostApplication($data: JobPostApplicationCreateInput!) {
+  createJobPostApplication(data: $data) {
+    ... on JobPostApplicationType {
+      id
+    }
+    ... on GraphQLErrorType {
+      message
+      code
+    }
+  }
+}
+    `,b=e=>(0,i.n)({mutationKey:["CreateJobPostApplication"],mutationFn:e=>p(v,e)(),...e});b.getKey=()=>["CreateJobPostApplication"],b.fetcher=(e,t)=>p(v,e,t);let f=`
+    mutation CreateJobPost($data: JobPostCreateInput!) {
+  jobPostCreate(data: $data) {
+    id
+  }
+}
+    `,x=e=>(0,i.n)({mutationKey:["CreateJobPost"],mutationFn:e=>p(f,e)(),...e});x.getKey=()=>["CreateJobPost"],x.fetcher=(e,t)=>p(f,e,t);let P=`
+    mutation getInterviewSumary($interviewId: UUID!) {
+  getSummary(interviewId: $interviewId) {
+    summary
+    communication
+    cultureFit
+    curiosity
+    experience
+    overallScore
+    problemSolving
+    technicalAbility
+  }
+}
+    `,A=e=>(0,i.n)({mutationKey:["getInterviewSumary"],mutationFn:e=>p(P,e)(),...e});A.getKey=()=>["getInterviewSumary"],A.fetcher=(e,t)=>p(P,e,t);let w=`
+    query Interview($id: GlobalID!) {
+  interviews(filters: {id: $id}) {
+    edges {
+      node {
+        id
+        questions {
+          question
+        }
+        user {
+          name
+        }
+        jobPostApplication {
+          jobPost {
+            company {
+              name
+            }
+            title
+          }
+        }
+      }
+    }
+  }
+}
+    `,g=(e,t)=>(0,o.I)({queryKey:["Interview",e],queryFn:p(w,e),...t});g.getKey=e=>["Interview",e],g.fetcher=(e,t)=>p(w,e,t);let I=`
+    query AppliedJobs {
+  appliedJobPosts {
+    applicationLimit
+    availibility
+    city
+    company {
+      name
+    }
+    country
+    createdAt
+    deletedAt
+    department
+    description
+    employmentType
+    endDate
+    expiresAt
+    firstPublishedAt
+    id
+    payType
+    startDate
+    salary
+    status
+    title
+    totalApplications
+    updatedAt
+    workMode
+    workStartDate
+  }
+}
+    `,q=(e,t)=>(0,o.I)({queryKey:void 0===e?["AppliedJobs"]:["AppliedJobs",e],queryFn:p(I,e),...t});q.getKey=e=>void 0===e?["AppliedJobs"]:["AppliedJobs",e],q.fetcher=(e,t)=>p(I,e,t);let E=`
+    query getAllSummaries {
+  summary {
+    summary {
+      summary
+    }
+    jobPost {
+      title
+    }
+    user {
+      email
+      lastName
+      firstName
+    }
+    interview {
+      scheduledAt
+    }
+  }
+}
+    `,C=(e,t)=>(0,o.I)({queryKey:void 0===e?["getAllSummaries"]:["getAllSummaries",e],queryFn:p(E,e),...t});C.getKey=e=>void 0===e?["getAllSummaries"]:["getAllSummaries",e],C.fetcher=(e,t)=>p(E,e,t);let T=`
+    query JobsPosts {
+  jobPosts {
+    applicationLimit
+    availibility
+    city
+    company {
+      name
+    }
+    country
+    createdAt
+    deletedAt
+    department
+    description
+    employmentType
+    endDate
+    expiresAt
+    firstPublishedAt
+    id
+    payType
+    startDate
+    salary
+    status
+    title
+    totalApplications
+    updatedAt
+    workMode
+    workStartDate
+  }
+}
+    `,j=(e,t)=>(0,o.I)({queryKey:void 0===e?["JobsPosts"]:["JobsPosts",e],queryFn:p(T,e),...t});j.getKey=e=>void 0===e?["JobsPosts"]:["JobsPosts",e],j.fetcher=(e,t)=>p(T,e,t);let S=`
+    query UnappliedJobs {
+  unappliedJobPosts {
+    applicationLimit
+    availibility
+    city
+    company {
+      name
+    }
+    country
+    createdAt
+    deletedAt
+    department
+    description
+    employmentType
+    endDate
+    expiresAt
+    firstPublishedAt
+    id
+    payType
+    startDate
+    salary
+    status
+    title
+    totalApplications
+    updatedAt
+    workMode
+    workStartDate
+  }
+}
+    `,J=(e,t)=>(0,o.I)({queryKey:void 0===e?["UnappliedJobs"]:["UnappliedJobs",e],queryFn:p(S,e),...t});J.getKey=e=>void 0===e?["UnappliedJobs"]:["UnappliedJobs",e],J.fetcher=(e,t)=>p(S,e,t);let _=`
+    query UpcomingInterview {
+  upcomingInterview {
+    id
+    scheduledAt
+    jobPostApplication {
+      jobPost {
+        applicationLimit
+        availibility
+        city
+        company {
+          name
+        }
+        country
+        createdAt
+        deletedAt
+        department
+        description
+        employmentType
+        endDate
+        expiresAt
+        firstPublishedAt
+        id
+        payType
+        startDate
+        salary
+        status
+        title
+        totalApplications
+        updatedAt
+        workMode
+        workStartDate
+      }
+    }
+  }
+}
+    `,k=(e,t)=>(0,o.I)({queryKey:void 0===e?["UpcomingInterview"]:["UpcomingInterview",e],queryFn:p(_,e),...t});function K(e,t){return t?Buffer.from(`${e}:${t}`).toString("base64"):""}function z(e){return e?Buffer.from(e,"base64").toString("ascii").split(":")?.[1]:""}k.getKey=e=>void 0===e?["UpcomingInterview"]:["UpcomingInterview",e],k.fetcher=(e,t)=>p(_,e,t)},74075:e=>{"use strict";e.exports=require("zlib")},78335:()=>{},79551:e=>{"use strict";e.exports=require("url")},79805:(e,t,r)=>{"use strict";r.r(t),r.d(t,{default:()=>i});let i=(0,r(12907).registerClientReference)(function(){throw Error("Attempted to call the default export of \"/home/mirza-zeeshan/Apriora/apriora/src/app/(interview)/interview/[id]/completed/page.tsx\" from the server, but it's on the client. It's not possible to invoke a client function from the server, it can only be rendered as a Component or passed to props of a Client Component.")},"/home/mirza-zeeshan/Apriora/apriora/src/app/(interview)/interview/[id]/completed/page.tsx","default")},81630:e=>{"use strict";e.exports=require("http")},87117:(e,t,r)=>{Promise.resolve().then(r.bind(r,4332))},94431:(e,t,r)=>{"use strict";r.r(t),r.d(t,{default:()=>s,metadata:()=>n});var i=r(37413);r(61135);var o=r(44314);let n={title:"Create Next App",manifest:"/manifest.json",description:"Generated by create next app"};function s({children:e}){return(0,i.jsx)("html",{lang:"en",children:(0,i.jsx)("body",{children:(0,i.jsx)(o.default,{children:e})})})}},96487:()=>{}};var t=require("../../../../../webpack-runtime.js");t.C(e);var r=e=>t(t.s=e),i=t.X(0,[51,811,929],()=>r(7816));module.exports=i})();
